6b3ff6fde9e3f4f440f85159e327caecd7ec3a9a
dale cornelius <dale.cornelius@wework.com>
2018-02-13 06:36:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  if int % 3 && 5 == 0
+  elsif int % 3 && 5 == 0
     puts "FizzBuss"
 end
